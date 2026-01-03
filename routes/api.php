aa934b90bbffe1e1824a322cff0ccbbad29e337f
Shinoy P B <pbshinoy@gmail.com>
2026-01-03 11:16:21 +0530

    api route fixe

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,15 @@
 <?php
 
-// Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
-//     // --------------- Register and Login ----------------//
-//     Route::post('register', 'AuthenticationController@register')->name('register');
-//     Route::post('login', 'AuthenticationController@login')->name('login');
+Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
+    // --------------- Register and Login ----------------//
+    Route::post('register', 'AuthenticationController@register')->name('register');
+    Route::post('login', 'AuthenticationController@login')->name('login');
     
-//     // ------------------ Get Data ----------------------//
-//     Route::middleware('auth:sanctum')->group(function () {
-//         Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
-//         Route::post('logout', 'AuthenticationController@logOut')->name('logout');
-//     });
-// });
-
-
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthenticationController;
-
-Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
-Route::post('/login', [AuthenticationController::class, 'login'])->name('login');
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/get-user', [AuthenticationController::class, 'userInfo']);
-    Route::post('/logout', [AuthenticationController::class, 'logOut']);
+    // ------------------ Get Data ----------------------//
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
+        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+    });
 });
+
+
